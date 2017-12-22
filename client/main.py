143571fca52b524e350469f0be8d280daaa45946
Kyle Combes <kylecombes@gmail.com>
2017-12-21 16:26:59 -0800

    Client reporting IP address, hostname, OS info, and WiFi status to server, which saves to MongoDB instance.

--- client/main.py
+++ client/main.py
@@ -1,6 +1,10 @@
 from os import environ
 import json
+import netifaces
+import platform
 import requests
+from wifi import Cell
+from wifi.exceptions import InterfaceError
 
 
 class IPReporter:
@@ -14,18 +18,62 @@ class IPReporter:
         self.server = server
 
     def report(self, data):
-        ip = '192.168.39.24'
-        hostname = 'hydra'
-        os_name = 'Ubuntu 16.04 64-bit'
-        requests.get(self.server + '/report-ip?ip={ip}&hostname={hostname}&os={os}'.format(ip=ip, hostname=hostname, os=os_name))
+        requests.post(self.server + '/report-ip', data=data)
+
+    def get_interfaces(self, json_encode=False, exclude_lo=True):
+        ifaces = []
+        for iface in netifaces.interfaces():
+            if exclude_lo and iface == 'lo':
+                continue
+            address = netifaces.ifaddresses(iface)[2][0]
+            iface_data = {
+                'name': iface,
+                'address': address['addr'],
+                'netmask': address['netmask']
+            }
+            try:  # If this interface is a WiFi interface, get its info
+                wifi = self.get_wifi_info(iface)
+                iface_data['wifi'] = wifi
+            except InterfaceError:
+                pass
+            ifaces.append(iface_data)
+
+        return json.dumps(ifaces) if json_encode else ifaces
+
+    def get_wifi_info(self, iface):
+        info = list(Cell.all(iface))[0]
+        res = {
+            'channel': info.channel,
+            'encryption': info.encryption_type,
+            'frequency': info.frequency,
+            'signalStrength': info.signal,
+            'ssid': info.ssid,
+            'quality': info.quality,
+        }
+        if info.noise:
+            res['noise'] = info.noise
+        return res
+
+    def get_os_info(self, json_encode=False):
+        res = {
+            'name': platform.system(),
+            'architecture': platform.machine()
+        }
+        if platform.system() == 'Linux':
+            res['dist'] = platform.linux_distribution()
+
+        return json.dumps(res) if json_encode else res
 
 
 if __name__ == '__main__':
     server = environ.get('IP_REPORTER_SERVER')
     if server:
         rep = IPReporter(server)
-        data = {'testing123': 'take it back, back!'}
-        data_json = json.dumps(data)
-        rep.report(data_json)
+        data = {
+            'interfaces': rep.get_interfaces(True),
+            'hostname': platform.node(),
+            'os': rep.get_os_info(True)
+        }
+        rep.report(data)
     else:
         print('Error: IP_REPORTER_SERVER environment variable not set')
--- server/index.mjs
+++ server/index.mjs
@@ -1,5 +1,7 @@
 import express from "express";
+import BodyParser from "body-parser";
 import http from "http";
+import MongoClient from "mongodb";
 import util from "util";
 import WebSocket from 'ws';
 
@@ -10,29 +12,121 @@ const port = 1996; // TODO Load this from an environment variable
 // Initialization
 
 const app = express();
+app.use(BodyParser.json()); // Support JSON-encoded bodies
+app.use(BodyParser.urlencoded()); // Support encoded bodies
 const server = http.Server(app);
 
 server.listen(port, () => {
     console.log(`Server listening on port ${port}...`);
 });
 
-// TODO Connect to MongoDB
 
-const router = express.Router();
-app.use(router);
+// Connect to the MongoDB instance
+const username = process.env.NETSTAT_MONGO_USERNAME;
+const password = process.env.NETSTAT_MONGO_PASSWORD;
+const mongoPort = process.env.NETSTAT_MONGO_PORT;
+const cluster1 = process.env.NETSTAT_MONGO_CLUSTER1;
+const cluster2 = process.env.NETSTAT_MONGO_CLUSTER2;
+const cluster3 = process.env.NETSTAT_MONGO_CLUSTER3;
+const dbName = process.env.NETSTAT_MONGO_DB_NAME;
+const replicaSet = process.env.NETSTAT_MONGO_REPLICA_SET;
+const authSource = process.env.NETSTAT_MONGO_AUTH_SOURCE || 'admin';
 
-router.get('/report-ip', (req, res) => {
-    console.log('Received post to /report-ip:');
-    console.log(util.inspect(req.query));
-    res.sendStatus(300);
+// Make sure all the necessary info is given
+if (!(username && password && cluster1 && cluster2 && cluster3 && dbName && replicaSet)) {
+    // Couldn't find environment variables
+    console.error('Please check your environment variables to make sure the following are defined:');
+    console.error('\t- NETSTAT_MONGO_USERNAME');
+    console.error('\t- NETSTAT_MONGO_PASSWORD');
+    console.error('\t- NETSTAT_MONGO_PORT');
+    console.error('\t- NETSTAT_MONGO_CLUSTER1');
+    console.error('\t- NETSTAT_MONGO_CLUSTER2');
+    console.error('\t- NETSTAT_MONGO_CLUSTER3');
+    console.error('\t- NETSTAT_MONGO_DB_NAME');
+    console.error('\t- NETSTAT_MONGO_REPLICA_SET');
+    console.error('Connection to MongoDB instance failed.');
+    process.exit(1); // Kill the server
+}
+// const mongoUri = `mongodb+srv://${username}:${password}@${cluster}`;
+// console.log('Connecting to MongoDB using ' + mongoUri);
+const mongoUri = `mongodb://${username}:${password}@${cluster1}:${mongoPort},${cluster2}:${mongoPort},${cluster3}:${mongoPort}/${dbName}?ssl=true&replicaSet=${replicaSet}&authSource=${authSource}`;
+
+// Connect to the MongoDB database
+MongoClient.connect(mongoUri, (err, db) => {
+    if (err) { // There was a problem connecting to the MongoDB instance
+        console.error('Error connecting to MongoDB instance:');
+        console.error(err);
+    } else { // Connected to db successfully
+        console.log('Successfully connected to MongoDB instance. Starting HTTP server...');
+
+        const router = express.Router();
+        app.use(router);
+
+        router.post('/report-ip', (req, res) => {
+            console.log('Received post to /report-ip:');
+
+            const data = parseReportingTransmission(req.body);
+            if (data) {
+                db.collection('devices').insertOne(data, (err, dbRes) => {
+                    if (err) {
+                        res.sendStatus(500);
+                        console.error(err);
+                    } else {
+                        res.sendStatus(300);
+                    }
+                });
+            } else {
+                res.sendStatus(400);
+                console.log('Bad Request status code (400) sent to client.');
+            }
+        });
+
+        console.log('API server started successfully. Starting WebSocket server...');
+
+        // Configure WebSocket server
+        const wss = new WebSocket.Server({server});
+        // const connectedClients = [];
+        wss.on('connection', (ws) => {
+            console.log('Client connected');
+        });
+        wss.on('close', () => {
+            console.log('Client disconnected');
+        });
+
+        console.log(`HTTP and WebSocket servers started successfully. Listening on port ${port}.`)
+    }
 });
 
-// Configure WebSocket server
-const wss = new WebSocket.Server({ server });
-// const connectedClients = [];
-wss.on('connection', (ws) => {
-    console.log('Client connected');
-});
-wss.on('close', () => {
-    console.log('Client disconnected');
-});
+function parseReportingTransmission(postData) {
+
+    const data = {}; // Data to be saved to db
+
+    // Pull out network interface info
+    let interfaces = postData['interfaces'];
+    if (interfaces) {
+        data['interfaces'] = JSON.parse(interfaces);
+    } else {
+        console.warn('Request missing interfaces info');
+        return null;
+    }
+
+    // Pull out OS info
+    let os = postData['os'];
+    if (os) {
+        data['os'] = JSON.parse(os);
+    } else {
+        console.warn('Request missing OS info');
+        return null;
+    }
+
+    // Pull out hostname
+    const hostname = postData['hostname'];
+    if (hostname) {
+        data['hostname'] = hostname;
+    } else {
+        console.warn('Request missing hostname.');
+        return null;
+    }
+
+    return data;
+}
